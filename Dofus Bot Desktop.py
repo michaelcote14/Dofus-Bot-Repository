25b8a77c219d4326452f3eaca4ea39ddf0eaabed
Michael Cote <michaelcote14@gmail.com>
2022-06-24 00:21:07 -0500

    Dofus Bot Reposity desktop version first commit.html

---
+++ Dofus Bot Desktop.py
@@ -0,0 +1,289 @@
+# Starting Map: (3, -23)
+import pyautogui
+import keyboard
+import time
+for i in range(500):
+    time.sleep(3.8)
+    print(pyautogui.position())
+    pyautogui.moveTo(1078, 844, 0.2)
+    pyautogui.click(1176, 574, 1, 1) #Middle left wheat
+    time.sleep(5)
+    if keyboard.is_pressed('Shift'):
+        print('killing program')
+    pyautogui.click(1275, 528, 1, 1) #Middle right wheat
+    time.sleep(2)
+    pyautogui.click(1461, 787, 1, 1) #Bottom right wheat 1
+    time.sleep(2)
+    if keyboard.is_pressed('Shift'):
+        print('killing program')
+    pyautogui.click(1399, 747, 1, 1) #Bottom right wheat 2
+    time.sleep(2)
+    if keyboard.is_pressed('Shift'):
+        print('killing program')
+    pyautogui.click(1469, 713, 1, 1) #Bottom right wheat 3
+    time.sleep(2)
+    if keyboard.is_pressed('Shift'):
+        print('killing program')
+    pyautogui.click(1545, 711, 1, 1) #Bottom right wheat 4
+    time.sleep(3)
+    pyautogui.click(1523, 675, 1, 1) #Bottom right wheat 5
+    time.sleep(3)
+    if keyboard.is_pressed('Shift'):
+        print('killing program')
+    pyautogui.click(1523, 675, 1, 1) #Bottom right wheat 6
+    time.sleep(3)
+    pyautogui.click(1141, 908, 1, 1)  # Move down one map
+    time.sleep(5)
+
+    #Map Location: 3, -22
+    pyautogui.click(948, 444, 1, 1)  #1
+    time.sleep(7)
+    if keyboard.is_pressed('Shift'):
+        print('killing program')
+    pyautogui.click(891, 424, 1, 1)  #2
+    time.sleep(5)
+    if keyboard.is_pressed('Shift'):
+        print('killing program')
+    pyautogui.click(846, 488, 1, 1)  #3
+    time.sleep(2)
+    pyautogui.click(894, 506, 1, 1)  #4
+    time.sleep(2)
+    if keyboard.is_pressed('Shift'):
+        print('killing program')
+    pyautogui.click(860, 536, 1, 1)  #5
+    time.sleep(2)
+    pyautogui.click(948, 546, 1, 1)  #6
+    time.sleep(3)
+    pyautogui.click(1014, 531, 1, 1) #7
+    time.sleep(3)
+    if keyboard.is_pressed('Shift'):
+        print('killing program')
+    pyautogui.click(979, 471, 1, 1)  #8
+    time.sleep(3)
+    pyautogui.click(1008, 383, 1, 1) #9
+    time.sleep(2)
+    pyautogui.click(1092, 415, 1, 1)  #10
+    time.sleep(5)
+    pyautogui.click(1676, 648, 1, 1)  #Go to right map
+    time.sleep(5)
+    if keyboard.is_pressed('Shift'):
+        print('killing program')
+    pyautogui.click(1739, 397, 1, 1)  #Go right again
+    time.sleep(5)
+
+    #Map Location: 5, -22
+    pyautogui.click(627, 300, 1, 1)  # Bottom wheat 1
+    time.sleep(5)
+    if keyboard.is_pressed('Shift'):
+        print('killing program')
+    pyautogui.click(597, 271, 1, 1)  # Bottom wheat 2
+    time.sleep(2)
+    if keyboard.is_pressed('Shift'):
+        print('killing program')
+    pyautogui.click(524, 216, 1, 1)  # Bottom wheat 3
+    time.sleep(2)
+    if keyboard.is_pressed('Shift'):
+        print('killing program')
+    pyautogui.click(624, 163, 1, 1)  # Bottom wheat 4
+    time.sleep(2)
+    pyautogui.click(726, 212, 1, 1)  # Bottom wheat 5
+    time.sleep(3)
+    pyautogui.click(813, 239, 1, 1)  # Bottom wheat 6
+    time.sleep(3)
+    if keyboard.is_pressed('Shift'):
+        print('killing program')
+    pyautogui.click(697, 199, 1, 1)  # Bottom wheat 7
+    time.sleep(3)
+    pyautogui.click(577, 221, 1, 1)  # Bottom wheat 8#
+    time.sleep(2)
+    if keyboard.is_pressed('Shift'):
+        print('killing program')
+    pyautogui.click(729, 264, 1, 1)  # Bottom wheat 9#
+    time.sleep(2)
+    pyautogui.click(589, 224, 1, 1)  # Bottom wheat 9#
+    time.sleep(5)
+    # 1st loop barley
+    time.sleep(4)
+    pyautogui.click(1513, 194, 1, 1)
+    time.sleep(5)
+    pyautogui.click(1371, 64, 1, 1)
+    time.sleep(2)
+    if keyboard.is_pressed('Shift'):
+        print('killing program')
+    pyautogui.click(1513, 134, 1, 1)
+    time.sleep(2)
+    pyautogui.click(1293, 104, 1, 1)
+    time.sleep(2)
+    pyautogui.click(1410, 143, 1, 1)
+    time.sleep(3)
+    pyautogui.click(1355, 124, 1, 1)
+    time.sleep(3)
+    if keyboard.is_pressed('Shift'):
+        print('killing program')
+    pyautogui.click(1316, 93, 1, 1)
+    time.sleep(2)
+    pyautogui.click(1463, 119, 1, 1)
+    time.sleep(5)
+
+    pyautogui.click(1813, 504, 1, 1) #Move right
+    time.sleep(5)
+    #Map 6, -22
+    pyautogui.click(679, 246, 1, 1)  #Fish
+    time.sleep(6)
+    pyautogui.click(1284, 552, 1, 1)
+    time.sleep(2)
+    if keyboard.is_pressed('Shift'):
+        print('killing program')
+    pyautogui.click(1316, 481, 1, 1)
+    time.sleep(2)
+    pyautogui.click(1223, 533, 1, 1)
+    time.sleep(3)
+    if keyboard.is_pressed('Shift'):
+        print('killing program')
+    pyautogui.click(1424, 489, 1, 1)
+    time.sleep(2)
+    pyautogui.click(1278, 508, 1, 1)
+    time.sleep(3)
+    pyautogui.click(1324, 537, 1, 1)
+    time.sleep(5)
+    if keyboard.is_pressed('Shift'):
+        print('killing program')
+
+
+
+
+
+
+    #Move Right Again
+    pyautogui.click(1702, 360, 1, 1)
+    time.sleep(5)
+    pyautogui.click(541, 258, 1, 1)  #Nettle
+    time.sleep(3)
+    if keyboard.is_pressed('Shift'):
+        print('killing program')
+    pyautogui.click(953, 34, 1, 1)
+    time.sleep(5)
+    if keyboard.is_pressed('Shift'):
+        print('killing program')
+
+
+    #Map Location: 7, -23
+    time.sleep(4)
+    pyautogui.click(687, 636, 1, 1)
+    time.sleep(5)
+    if keyboard.is_pressed('Shift'):
+        print('killing program')
+    pyautogui.click(750, 653, 1, 1)
+    time.sleep(3)
+    if keyboard.is_pressed('Shift'):
+        print('killing program')
+    pyautogui.click(825, 616, 1, 1)
+    time.sleep(2)
+    pyautogui.click(754, 581, 1, 1)
+    time.sleep(2)
+    if keyboard.is_pressed('Shift'):
+        print('killing program')
+    pyautogui.click(800, 561, 1, 1)
+    time.sleep(2)
+    pyautogui.click(1083, 265, 1, 1)
+    time.sleep(3)
+    pyautogui.click(1015, 211, 1, 1)
+    time.sleep(3)
+    pyautogui.click(1046, 159, 1, 1)
+    time.sleep(3)
+    if keyboard.is_pressed('Shift'):
+        print('killing program')
+    pyautogui.click(1181, 170, 1, 1)
+    time.sleep(2)
+    pyautogui.click(1150, 204, 1, 1)  # Bottom wheat 6
+    time.sleep(3)
+    pyautogui.click(1083, 209, 1, 1)
+    time.sleep(2)
+    pyautogui.click(1070, 172, 1, 1)
+    time.sleep(3)
+    if keyboard.is_pressed('Shift'):
+        print('killing program')
+    pyautogui.click(909, 142, 1, 1)
+    time.sleep(5)
+
+
+    #Move Right
+    pyautogui.click(1723, 512, 1, 1)
+    time.sleep(5)
+    pyautogui.click(1723, 512, 1, 1)
+    time.sleep(7)
+    # Map Location: 9, -23
+    pyautogui.click(807, 442, 1, 1)
+    time.sleep(5)
+    if keyboard.is_pressed('Shift'):
+        print('killing program')
+    pyautogui.click(687, 522, 1, 1)
+    time.sleep(2)
+    if keyboard.is_pressed('Shift'):
+        print('killing program')
+    pyautogui.click(824, 498, 1, 1)
+    time.sleep(2)
+    pyautogui.click(730, 486, 1, 1)
+    time.sleep(2)
+    pyautogui.click(763, 517, 1, 1)
+    time.sleep(3)
+    if keyboard.is_pressed('Shift'):
+        print('killing program')
+    pyautogui.click(764, 475, 1, 1)
+    time.sleep(5)
+    pyautogui.click(890, 84, 1, 1)
+
+    # Move back to start
+    time.sleep(6)
+    pyautogui.click(121, 389, 1, 1)
+    time.sleep(5)
+    if keyboard.is_pressed('Shift'):
+        print('killing program')
+    pyautogui.click(121, 500, 1, 1)
+    time.sleep(10)
+    pyautogui.click(121, 389, 1, 1)
+    time.sleep(5)
+    pyautogui.click(121, 389, 1, 1)
+    time.sleep(5)
+    if keyboard.is_pressed('Shift'):
+        print('killing program')
+    pyautogui.click(200, 719, 1, 1)
+    time.sleep(8)
+    # Map 4, -23
+    time.sleep(3.5)
+    pyautogui.click(1021, 209, 1, 1)
+    time.sleep(5)
+    pyautogui.click(955, 166, 1, 1)
+    time.sleep(2)
+    if keyboard.is_pressed('Shift'):
+        print('killing program')
+    pyautogui.click(762, 124, 1, 1)
+    time.sleep(2)
+    pyautogui.click(857, 79, 1, 1)
+    time.sleep(2)
+    pyautogui.click(985, 59, 1, 1)
+    time.sleep(3)
+    if keyboard.is_pressed('Shift'):
+        print('killing program')
+    pyautogui.click(864, 114, 1, 1)
+    time.sleep(3)
+    pyautogui.click(841, 148, 1, 1)
+    time.sleep(3)
+    pyautogui.click(808, 95, 1, 1)
+    time.sleep(2)
+    pyautogui.click(937, 116, 1, 1)
+    time.sleep(5)
+    if keyboard.is_pressed('Shift'):
+        print('killing program')
+    pyautogui.click(895, 184, 1, 1)
+    time.sleep(5)
+    # Move left
+    pyautogui.click(121, 389, 1, 1)
+    time.sleep(5)
+
+
+
+
+#ToDo put in response to invites using group chat and pyautogui.typewrite() + click on chat bar
+#ToDO put in inventory check then go to bank
+#ToDo put in fighter bot
